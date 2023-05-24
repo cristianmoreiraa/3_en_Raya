2e942a8b5f1f10fe7519a4b3bd7ed3dbe2b97140
JorgeUsatorre <usatorre1210@gmail.com>
2023-05-24 11:26:19 +0200

    Subo la posible clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import vista.*;
+import controler.*;
+import modelo.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        //nuevas instancias de clase
+        Juego modelo = new Juego();
+        Game vista = new Game();
+        controlerGame controler = new controlerGame( vista , modelo );
+        controler.iniciar_vista();
+        vista.setVisible(true);
     }
 }
